05acbe8e42d282d3ee208b6363293105d79b0ec0
baggiowangyu <baggiowangyu@gmail.com>
2019-03-18 00:36:58 +0800

    完成SSH客户端开发，准备进行Docker接口封装

--- GxxGmSSHDemo/GxxGmSSHDemo.cpp
+++ GxxGmSSHDemo/GxxGmSSHDemo.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 #include "..\GxxGmSSH\GxxGmSSH.h"
 
 #pragma comment(lib, "GxxGmSSH.lib")
@@ -9,6 +10,44 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	GxxGmSSH ssh;
+
+	int errCode = ssh.Initialize();
+	if (errCode != 0)
+	{
+		std::cout<<"³õÊ¼»¯Ê§°Ü£¡´íÎóÂë£º"<<errCode<<std::endl;
+
+		system("pause");
+		return errCode;
+	}
+
+	errCode = ssh.Login("192.168.231.139", "root", "871511");
+	if (errCode != 0)
+	{
+		std::cout<<"µÇÂ¼Ê§°Ü£¡´íÎóÂë£º"<<errCode<<std::endl;
+
+		system("pause");
+		return errCode;
+	}
+
+	while (true)
+	{
+		std::string command;
+		std::cin.clear();
+		std::cin>>command;
+
+		std::string result;
+		errCode = ssh.Exec(command.c_str(), result);
+		if (errCode != 0)
+		{
+			std::cout<<"ÃüÁîÖ´ÐÐÊ§°Ü£¡´íÎóÂë£º"<<errCode<<std::endl;
+			break;
+		}
+
+		std::cout<<result.c_str()<<std::endl;
+	}
+
+	system("pause");
 	return 0;
 }
 
